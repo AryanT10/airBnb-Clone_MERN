26e189b4eb0a82359c8feea5ee324e53dcc708e8
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-09-11 20:53:21 -0400

    feat: adding places

--- client/src/pages/AccountPage.jsx
+++ client/src/pages/AccountPage.jsx
@@ -30,9 +30,12 @@ export default function AccountPage() {
 
 
 	function linkClasses(type = null) {
-		let classes = 'py-2 px-6';
+		let classes = 'inline-flex gap-1 py-2 px-6 rounded-full';
 		if (type === subpage) {
 			classes += ' bg-primary text-white rounded-full';
+		}
+		else {
+			classes += ' bg-gray-200'
 		}
 		return classes
 	}
