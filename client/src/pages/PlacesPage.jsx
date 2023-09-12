d03a644ad0ffc7885753d8e9e702053b2b9828cc
atuwar@myseneca.ca <atuwar@myseneca.ca>
2023-09-11 23:40:28 -0400

    feat: checkpoint , upload by link

--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import Perks from '../Perks';
+import axios from 'axios';
 
 export default function PlacesPage() {
 	const { action } = useParams();
@@ -23,6 +24,15 @@ export default function PlacesPage() {
 
 	function inputParagraph(text) {
 		<p className="text-gray-500 text-sm">{text}</p>
+	}
+
+	async function addPhoto(e) {
+		e.preventDefault();
+		const { data: filename } = await axios.post('/upload-by-link', { link: photoLink })
+		setAddedPhotos(prev => {
+			return [...prev, filename];
+		});
+		setPhotoLink('');
 	}
 
 	function preInput(header, paragraph) {
@@ -50,19 +60,24 @@ export default function PlacesPage() {
 					<form>
 						{/* Title */}
 						{preInput('Title', 'title for your place. short and catchy as in advertisement')}
-						<input type="text" value={title} onChange={e => setTitle(e.target.title)} placeholder="title, for example : Lovely Apartment" />
+						<input type="text" value={title} onChange={e => setTitle(e.target.value)} placeholder="title, for example : Lovely Apartment" />
 
 						{/* Address */}
 						{preInput('Address', 'Address of this place')}
-						<input type="text" value={address} onChange={e => setAddress(e.target.address)} placeholder="address" />
+						<input type="text" value={address} onChange={e => setAddress(e.target.value)} placeholder="address" />
 
 						{/* Photos */}
 						{preInput('Photos', 'photos to this place')}
 						<div className="flex gap-2">
-							<input value={photoLink} onChange={e => setPhotoLink(e.target.photoLink)} type="text" placeholder={'Add using a link ...jpg'} />
-							<button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;Photo</button>
+							<input value={photoLink} onChange={e => setPhotoLink(e.target.value)} type="text" placeholder={'Add using a link ...jpg'} />
+							<button onClick={addPhoto} className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;Photo</button>
 						</div>
 						<div className="mt-2 grid grid-cols-3 md:lg-grid-cols-4 lg:grid-cols-6 ">
+							{addedPhotos.length > 0 && addedPhotos.map((link,index) => (
+								<div key={index}>
+									{link}
+								</div>
+							))}
 							<button className=" flex justify-center gap-1 border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
 								<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
 									<path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
@@ -73,7 +88,7 @@ export default function PlacesPage() {
 
 						{/* Descriptions */}
 						{preInput('Description', 'Description of this place')}
-						<textarea value={description} onChange={e => setDescription(e.target.description)} />
+						<textarea value={description} onChange={e => setDescription(e.target.value)} />
 
 						{/* Perks: All perks sent to the Perks components */}
 						{preInput('Perks', 'Select all Perks of this place')}
@@ -83,7 +98,7 @@ export default function PlacesPage() {
 
 						{/* Extra Info */}
 						{preInput('Extra Information', 'house Rules etc')}
-						<textarea value={extraInfo} onChange={e => setExtraInfo(e.target.setExtraInfo)} />
+						<textarea value={extraInfo} onChange={e => setExtraInfo(e.target.value)} />
 
 						{/* Check-In, Check-Out, Max-Guests */}
 						{preInput('Check-In & Out, Max-Guests', 'Perks of this place')}
@@ -93,7 +108,7 @@ export default function PlacesPage() {
 								<h3>Check-In Time</h3>
 								<input type="text"
 									value={checkIn}
-									onChange={e => setCheckIn(e.target.setCheckIn)}
+									onChange={e => setCheckIn(e.target.value)}
 									placeholder="14" />
 							</div>
 
@@ -101,7 +116,7 @@ export default function PlacesPage() {
 								<h3>Check-Out Time</h3>
 								<input type="text"
 									value={checkOut}
-									onChange={e => setCheckOut(e.target.setCheckOut)}
+									onChange={e => setCheckOut(e.target.value)}
 									placeholder="11" />
 							</div>
 
@@ -109,7 +124,7 @@ export default function PlacesPage() {
 								<h3>Max-Guests</h3>
 								<input type="number"
 									value={maxGuests}
-									onChange={e => setMaxGuests(e.target.setMaxGuests)}/>
+									onChange={e => setMaxGuests(e.target.value)} />
 							</div>
 						</div>
 						<div>
